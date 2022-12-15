bea2d10b6a0b5e741b29c00f02a1060df764c507
Daria <dariabatiuk5@gmail.com>
2022-12-15 15:46:39 -0400

    changed timer + sorted high scores

--- js/script.js
+++ js/script.js
@@ -11,21 +11,17 @@ let option2 = document.querySelector("#option2");
 let option3 = document.querySelector("#option3");
 let option4 = document.querySelector("#option4");
 
-
 let total_correct = document.querySelector("#total_correct");
 let next_question = document.querySelector("#next_question");
-
 
 let result = document.querySelector("#result");
 let points = document.querySelector("#points");
 
 let choice_que = document.querySelectorAll(".choice_que");
 
-
 let submit = document.querySelector("#submit");
 let view = document.querySelector("#view");
 let back = document.querySelector("#score__back");
-
 
 let index = 0;
 let timer = 0;
@@ -39,140 +35,138 @@ let userScore = document.querySelector("#user__score");
 let highScoreeList = [];
 
 let countDown = () => {
-    if (timer === 0 || timer < 0) {
-        clearInterval(interval);
-        next_question.click();
-    } 
-    else {
-        timer--;
-        time.innerText = timer;
-    }
-}
+  if (timer === 0 || timer < 0) {
+    clearInterval(interval);
+    //next_question.click();
+		quiz.style.display = "none";
+    points.innerHTML = `Your final score is ${correct}`;
+    result.style.display = "block";
+  } else {
+    timer--;
+    time.innerText = timer;
+  }
+};
 
 let loadData = () => {
-    questionNo.innerText = index + 1 + ". ";
-    questionText.innerText = MCQS[index].question;
-    option1.innerText = MCQS[index].choice1;
-    option2.innerText = MCQS[index].choice2;
-    option3.innerText = MCQS[index].choice3;
-    option4.innerText = MCQS[index].choice4;
+  questionNo.innerText = index + 1 + ". ";
+  questionText.innerText = MCQS[index].question;
+  option1.innerText = MCQS[index].choice1;
+  option2.innerText = MCQS[index].choice2;
+  option3.innerText = MCQS[index].choice3;
+  option4.innerText = MCQS[index].choice4;
 
-    //    timer start
-    timer = 40;
-}
+  //    timer start
+  timer = 60;
+	interval = setInterval(countDown, 10000);
+};
 
 loadData();
 
 start.addEventListener("click", () => {
-    quiz.style.display = "block";
-    start__wrapper.style.display = "none";
-    
-    interval = setInterval(countDown, 1000);
-    loadData();
+  quiz.style.display = "block";
+  start__wrapper.style.display = "none";
 
-    choice_que.forEach(removeActive => {
-        removeActive.classList.remove("active");
-    })
+  interval = setInterval(countDown, 1000);
+  loadData();
 
-    total_correct.innerHTML = `${correct = 0} Out Of ${MCQS.length} Questions`;
+  choice_que.forEach((removeActive) => {
+    removeActive.classList.remove("active");
+  });
+
+  total_correct.innerHTML = `${(correct = 0)} Out Of ${MCQS.length} Questions`;
 });
 
 choice_que.forEach((choices, choiceNo) => {
-    choices.addEventListener("click", () => {
-        choices.classList.add("active");
-        if (choiceNo === MCQS[index].answer) {
-            correct++;
-            total_correct.innerHTML = `Correct!`;
-            total_correct.style.display = "block";
-            clearInterval(interval);
-        
-            for (i = 0; i <= 3; i++) {
-                choice_que[i].classList.add("disabled");
-            }
-        } else {
-            correct += 0;
-            total_correct.innerHTML = `Wrong!`;
-            total_correct.style.display = "block";
-            timer -= 10;
-        }
-        
-    })
+  choices.addEventListener("click", () => {
+    choices.classList.add("active");
+    if (choiceNo === MCQS[index].answer) {
+      correct++;
+      total_correct.innerHTML = `Correct!`;
+      total_correct.style.display = "block";
+      clearInterval(interval);
+
+      for (i = 0; i <= 3; i++) {
+        choice_que[i].classList.add("disabled");
+      }
+    } else {
+      correct += 0;
+      total_correct.innerHTML = `Wrong!`;
+      total_correct.style.display = "block";
+      timer -= 10;
+    }
+  });
 });
 
-
 next_question.addEventListener("click", () => {
-    if (index !== MCQS.length - 1) {
-        index++;
-        choice_que.forEach(removeActive => {
-            removeActive.classList.remove("active");
-        })      
-        total_correct.style.display = "block";
-        total_correct.innerHTML = `${correct} Out Of ${MCQS.length} Questions`;
-        clearInterval(interval);
-        interval = setInterval(countDown, 1000);
+  if (index !== MCQS.length - 1) {
+    index++;
+    choice_que.forEach((removeActive) => {
+      removeActive.classList.remove("active");
+    });
+    total_correct.style.display = "block";
+    total_correct.innerHTML = `${correct} Out Of ${MCQS.length} Questions`;
+    //clearInterval(interval);
+    //interval = setInterval(countDown, 1000);
 
-        loadData();
-    } else {
-        index = 0;
+    loadData();
+  } else {
+    index = 0;
 
-        clearInterval(interval);
-        quiz.style.display = "none";
-        points.innerHTML = `Your final score is ${correct}`;
-        result.style.display = "block";
-    }
-    for (i = 0; i <= 3; i++) {
-        choice_que[i].classList.remove("disabled");
-    }
-})
+    clearInterval(interval);
+    quiz.style.display = "none";
+    points.innerHTML = `Your final score is ${correct}`;
+    result.style.display = "block";
+  }
+  for (i = 0; i <= 3; i++) {
+    choice_que[i].classList.remove("disabled");
+  }
+});
 
+function saveScore(initialsValue) {
+  var scoreObj = {
+    name: initialsValue,
+    score: correct,
+  };
 
-function saveScore(initialsValue){
+  let scores = JSON.parse(window.localStorage.getItem("highScores") || "[]");
+  scores.push(scoreObj);
+  scores.sort((a, b) => b.score - a.score);
+  scores.splice(5);
 
-    var scoreObj = {
-        name: initialsValue,
-        score: correct
-    }
-    
-    let scores = JSON.parse(window.localStorage.getItem('highScores') || "[]");
-    scores.push(scoreObj);
-
-    window.localStorage.setItem('highScores', JSON.stringify(scores));
+  window.localStorage.setItem("highScores", JSON.stringify(scores));
 }
 
 //What happens when you click 'Submit'
 submit.addEventListener("click", () => {
-        // event.preventDefault();
+  // event.preventDefault();
 
-        result.style.display = "none";
-        score.style.display = "block";
+  result.style.display = "none";
+  score.style.display = "block";
 
-        var initialsValue = initials.value.trim();
-        saveScore(initialsValue);
-        
+  var initialsValue = initials.value.trim();
+  saveScore(initialsValue);
 });
 
+view.addEventListener("click", function (event) {
+  event.preventDefault();
+  start__wrapper.style.display = "none";
+  console.log(view);
+  score.style.display = "block";
 
-view.addEventListener("click", function(event) {
-        event.preventDefault();
-        start__wrapper.style.display = "none";
-        console.log(view);
-        score.style.display = "block";
+  var scores = JSON.parse(window.localStorage.getItem("highScores") || "[]");
+  // scores.score.sort((a, b) => b - a);
 
-        var scores = JSON.parse(window.localStorage.getItem('highScores') || "[]");
-  
-        userInitials.innerHTML = "";
-        scores.forEach (score => {
-            let liName = document.createElement ("li");
-            liName.textContent = score.name + " : " + score.score;
-            userInitials.appendChild(liName);
-        }); 
-        
+  console.log(scores);
 
+  userInitials.innerHTML = "";
+  scores.forEach((score) => {
+    let liName = document.createElement("li");
+    liName.textContent = score.name + " : " + score.score;
+    userInitials.appendChild(liName);
+  });
 });
 
 back.addEventListener("click", () => {
-    score.style.display = "none";
-    start__wrapper.style.display = "flex";
+  score.style.display = "none";
+  start__wrapper.style.display = "flex";
 });
-
-
